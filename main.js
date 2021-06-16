3763ef1e8b1537ead2092fb9e96b9291803c86c0
Bunny Blake <sonicforest@gmail.com>
2021-06-15 22:38:22 -0400

    card class

---
+++ main.js
@@ -0,0 +1,22 @@
+class Card {
+	constructor(cardType, cardValue, cardText) {
+		this.cardType = cardType;
+		this.cardValue = cardValue;
+		this.cardText = cardText;
+	}
+}
+
+class Deck {
+	constructor() {
+		this.cards = [];
+	}
+
+	createDeck() {
+
+	}
+
+	shuffleDeck() {
+
+	}
+}
+
