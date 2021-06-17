735a34c436757b6c2797809193ae380b7c0eacd2
Bunny Blake <sonicforest@gmail.com>
2021-06-16 20:24:12 -0400

    basic classes outlined

--- main.js
+++ main.js
@@ -25,7 +25,38 @@ class Deck {
 	}
 
 	shuffleDeck() {
-
+		let card1, card2, tmp;
+		for (let i = 0; i < 1000; i++) {
+			card1 = Math.floor(Math.random() * this.cards.length);
+			card2 = Math.floor(Math.random() * this.cards.length);
+			tmp = this.cards[card1];
+			this.cards[card1] = this.cards[card2];
+			this.cards[card2] = tmp;
+		}
 	}
 }
 
+class Player {
+	constructor(name) {
+		this.playerName = name;
+		this.playerScore = 0;
+		this.playerCards = [];
+	}
+}
+
+class Board {
+	constructor() {
+		this.cardsInMiddle = [];
+		this.players = [];
+	}
+	start (playerOneName, playerTwoName) {
+		this.players.push(new Player(playerOneName));
+		this.players.push(new Player(playerTwoName));
+		let d = new Deck();
+		d.createDeck();
+		d.shuffleDeck();
+	}
+}
+
+let gameBoard = new Board();
+gameBoard.start('Rutiger', 'Jamila');
